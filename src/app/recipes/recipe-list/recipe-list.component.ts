489650e8507cb999f9de71fbd924af439ad4b7a4
Luke <nycthbre@gmail.com>
2022-07-31 01:34:46 +0200

    More templates editing

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('A test Recipe', 'This is simply a test.', 'https://learnenglishteens.britishcouncil.org/sites/teens/files/a_recipe_1.jpg'),
+    new Recipe('A test Recipe', 'This is simply a test.', 'https://learnenglishteens.britishcouncil.org/sites/teens/files/a_recipe_1.jpg'),
+    new Recipe('A test Recipe', 'This is simply a test.', 'https://learnenglishteens.britishcouncil.org/sites/teens/files/a_recipe_1.jpg')
+  ];
 
   constructor() { }
 
